9dd58d74d557f4a4efa3c9480d748aa62c88f853
masabino <masabinon@gmail.com>
2024-10-25 12:20:19 -0300

    2510

---
+++ escola/src/Paginas/Consulta.jsx
@@ -0,0 +1,8 @@
+export default function Consulta()
+{
+    return (
+        <div>
+            <h1>Consultas de Alunos</h1>
+        </div>
+    )
+}
---
+++ escola/src/Paginas/Exclusao.jsx
@@ -0,0 +1,8 @@
+export default function Exclusao()
+{
+    return (
+        <div>
+            <h1>Exclusao de Alunos</h1>
+        </div>
+    )
+}
